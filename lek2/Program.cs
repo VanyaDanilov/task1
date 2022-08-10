03e860c2111d04aa28e075b5eb4fc2c1e809b4b9
Vanya <vanya.danilov.95@bk.ru>
2022-08-10 22:37:13 +0500

    lek

---
+++ lek2/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = new Random().Next(100,1000);
+int num1 = number % 10 ;
+int num3 = number / 100;
+int result = num3 * 10 + num1;
+Console.WriteLine ($"Число: {result} из {number}");
+
+
+
+
+
+
+ 
