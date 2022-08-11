7b1336c9778af96713d6b678535fcf75ca646c96
Vanya <vanya.danilov.95@bk.ru>
2022-08-11 20:07:53 +0500

    lek2(2)

---
+++ lek2(2)/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if(num % 7 == 0 && num % 23 == 0){
+Console.WriteLine("Yes");
+}
+
+else{
+Console.WriteLine("No");
+}
