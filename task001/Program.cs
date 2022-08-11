3bf694ec7889eb1461bd8e7405f601c8605d6e57
Vanya <vanya.danilov.95@bk.ru>
2022-08-11 20:58:46 +0500

    task001

--- task001/Program.cs
+++ task001/Program.cs
@@ -2,12 +2,14 @@
             int num1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите второе число: ");
             int num2 = Convert.ToInt32(Console.ReadLine());
- 
-            if(num1 > num2)
-            {
-                Console.WriteLine("Первое число больше второго");
-            }
-            else if (num1 < num2)
+             num1 = num2;
+            Console.WriteLine("Равны");
+
+            if(num1 < num2)
             {
                 Console.WriteLine("Второе число больше первого");
+            }
+            else
+            {
+                Console.WriteLine("Первое число больше второго");
             }
