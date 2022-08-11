53d26c56e570569b34105ac9074324560a3be8b4
Vanya <vanya.danilov.95@bk.ru>
2022-08-11 19:40:07 +0500

    lek2(1)

---
+++ lek2(1)/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите 2 числа ");
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+if(num1 % num2 == 0){
+Console.WriteLine("Число 2 кратно числу 1");
+}
+else{
+    Console.WriteLine($"Число 2 не кратно числу 1, остаток от деления {num1 % num2}");
+}
