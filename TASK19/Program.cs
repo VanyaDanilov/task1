dacfc23aa22341de8cf623bd075ddc38748b450b
Vanya <vanya.danilov.95@bk.ru>
2022-08-16 20:59:20 +0500

    TASK19

---
+++ TASK19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите пятизначное число: ");
+int num = int.Parse(Console.ReadLine());
+int sqr1 = num / 10000; 
+int sqr2 = num % 100; int sqr5 = sqr2 /10;
+int sqr3 = num / 1000; int sqr6 = sqr3 % 10;
+int sqr4 = num % 10;
+if(sqr1 == sqr4 && sqr5 == sqr6) 
+{
+    Console.WriteLine("Да");
+    Console.WriteLine(num);
+}
+
+else
+{
+    Console.WriteLine("Нет");
+}
