0c127aa781443d8ed1fc82c5f4843a2b264a4f76
Vanya <vanya.danilov.95@bk.ru>
2022-08-16 08:52:25 +0500

    lek3(1)

---
+++ lek3(1)/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите x: ");
+int x = int.Parse(Console.ReadLain());
+Console.Write("Введите y: ");
+int y = int.Parse(Console.ReadLain());
+
+if( x > 0 && y > 0)
+{
+    Console.Write("1");
+}
+
+if(x < 0 && y > 0)
+{
+    Console.Write("2");  
+}
+
+if(x < 0 && y < 0)
+{
+    Console.Write("3");   
+}
+
+if(x > 0 && y < 0)
+{
+    Console.Write("4");  
+}
