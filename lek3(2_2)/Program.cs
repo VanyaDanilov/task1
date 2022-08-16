5369253c3d481b2a4fccaf66f52d9c6bcf259b2c
Vanya <vanya.danilov.95@bk.ru>
2022-08-16 09:27:36 +0500

    lek3(2_2 case)

---
+++ lek3(2_2)/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите номер четверти: ");
+int num = int.Parse(Console.ReadLine());
+switch (num)
+{
+    case 1:
+    {
+     Console.WriteLine("x > 0  y > 0");
+    break;
+    }
+    case 2:
+    {
+     Console.WriteLine("x < 0  y > 0");
+     break;
+    }
+    case 3:
+    {
+      Console.WriteLine("x < 0  y < 0");
+      break;  
+    }
+    case 4:
+    {
+       Console.WriteLine("x > 0  y < 0"); 
+       break;
+    }
+    default:
+    {
+        Console.WriteLine("Введена неправильная четверть ");
+        break;
+    }
+}
