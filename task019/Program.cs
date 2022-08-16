d07e7670d5e095060ca021344e59fe27418ff3eb
Vanya <vanya.danilov.95@bk.ru>
2022-08-16 11:16:00 +0500

    task19

---
+++ task019/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int num = int.Parse(Console.Readline());
+sqr1 = num % 10;
+sqr2 = num / 10;
+if(sqr1 == sqr2) 
+{
+    Console.WriteLine("Да");
+}
+
+else
+{
+    Console.WriteLine("Нет");
+}
