c3d31c1da33e43bb292f91ef67727553aaeb6202
Vanya <vanya.danilov.95@bk.ru>
2022-08-16 21:00:38 +0500

    TASK21

---
+++ TASK21/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine($"Введите координаты точки A ( x1, y1, z1)");
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+int z1 = int.Parse(Console.ReadLine());
+Console.WriteLine($"Введите координаты точки B (x2 , y2, z2)");
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+int z2 = int.Parse(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine($"Растояние между двумя точками на плоскости равно {distance:f3}");
