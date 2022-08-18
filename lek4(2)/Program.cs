685f91081d6276562441bae4515bc58fae02f779
Vanya <vanya.danilov.95@bk.ru>
2022-08-18 17:24:00 +0500

    lek4(task2)

---
+++ lek4(2)/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int DigitCount(int a)
+{
+    int i = 0;
+    while(a > 0)
+    {
+     i++;
+     a /= 10;
+    }
+    return i;
+}
+Console.WriteLine(DigitCount(num));
