f68ca33c2c6b9095b6f878a81696bfa8f1e43c7b
Vanya <vanya.danilov.95@bk.ru>
2022-08-18 17:39:43 +0500

    lek4(task3)

---
+++ lek4(3)/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+//4 -> 24 
+//5 -> 120
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(MultiDigital(num));
+int MultiDigital(int N)
+{
+    int a = 1;
+    for(int i = 1; i <= N; i++)
+    {
+        a = a * i;
+    }
+    return a;
+}
