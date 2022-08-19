efaea1799f3956b6fac4e45dfaf420b396f90c49
Vanya <vanya.danilov.95@bk.ru>
2022-08-19 10:01:36 +0500

    TASK29

---
+++ TASK29/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из N элементов, заполненных случайными числами из [a, b) и выводит их на экран.
+//5, 0, 20 -> [1, 2, 5, 7, 19]
+//3, 1, 35 -> [6, 1, 33]
+
+Console.WriteLine("Введите число (N) задающих количество элементов массива: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите начальное значение (a): ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите конечное значение (b): ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+void RandArray()
+{
+    int[] arr = new int[numN];
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(numA,numB);
+    }
+    Console.WriteLine($"[{String.Join(",", arr)}]");
+}
+RandArray();
