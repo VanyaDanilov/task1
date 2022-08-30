495500d7b13e7ab4ba348f2f04764cbc8c0660b2
Vanya <vanya.danilov.95@bk.ru>
2022-08-30 10:58:27 +0500

    lek5(2)

---
+++ lek5(2)/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+ int[] GetArray(int size, int minValue,  int maxValue)
+    {
+        int[] res = new int[size];
+
+        for(int i = 0; i < size; i++)
+        {
+            res[i] = new Random().Next(minValue, maxValue + 1);
+        }
+        return res;
+    }
+
+    int[] Rew(int[] mas)
+    {
+       for(int i = 0; i < mas.Length; i++)
+       {
+        mas[i] *= -1;
+       }
+       return mas;
+    }
+
+    int[] array = GetArray(5, -10, 11);
+    Console.WriteLine(String.Join(" ", array));
+    Console.WriteLine(String.Join(" ", Rew(array)));
